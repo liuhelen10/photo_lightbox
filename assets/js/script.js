837c80476b6c04f69e1f247500f21bc5107d3941
Helen Liu <liuhelen10@gmail.com>
2016-10-06 22:25:18 -0700

    get API key working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,23 @@
 function initializePage() {
+  var apiKey = '2a2a42163497badb56857f270528dd43';
+
   document.addEventListener('DOMContentLoaded', onDOMLoad);
 
-  function onDomLoad() {
-    console.log('hello');
+  function onDOMLoad() {
+    var xhttp = new XMLHttpRequest();
+    var apiUrl = 'https://api.flickr.com/services/rest/' +
+      '?method=flickr.photos.search' +
+      '&format=json&nojsoncallback=1&media=photos&per_page=104' +
+      '&api_key=' + apiKey +
+      '&text=puppies';
+
+    xhttp.onload = onSearchResultsLoaded;
+    xhttp.open('GET', apiUrl, true);
+    xhttp.send();
+  }
+
+  function onSearchResultsLoaded(e) {
+    console.log('here');
   }
 }
 
